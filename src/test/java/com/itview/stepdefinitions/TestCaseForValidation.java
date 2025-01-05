1f1699d74b7c86f0f6e18b4d5735a6f34c299945
AutomationTest001 <automationtest307@gmail.com>
2025-01-05 11:28:50 +0530

    Added One missing test case

---
+++ src/test/java/com/itview/stepdefinitions/TestCaseForValidation.java
@@ -0,0 +1,12 @@
+package com.itview.stepdefinitions;
+
+public final class TestCaseForValidation {
+
+	public static void main(String[] args) {
+
+
+		System.out.println("TestCaseForValidation");
+
+	}
+
+}
